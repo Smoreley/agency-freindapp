59dbdf1e01faaf1eff2274b37bfd511efd2153f8
skyler <Smoreley@gmail.com>
2016-11-08 19:36:08 -0500

    Tables

--- db/migrate/20140627005245_create_users.rb
+++ db/migrate/20140627005245_create_users.rb
@@ -4,8 +4,11 @@ class CreateUsers < ActiveRecord::Migration
         t.string :name
         t.string :provider
         t.string :uid
-
         t.string :profile_image
+        
+        t.integer :group_id
+        
+        t.references :user, foreign_key: true
         t.timestamps
     end
   end
---
+++ spec/factories/questions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :question do
+    inquire "MyString"
+    image "MyString"
+  end
+end
