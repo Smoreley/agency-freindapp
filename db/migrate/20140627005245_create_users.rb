d672f49cc6bd1c40c1b088c19e220ee10e79c559
skyler <Smoreley@gmail.com>
2016-11-08 19:38:20 -0500

    Update

--- db/migrate/20140627005245_create_users.rb
+++ db/migrate/20140627005245_create_users.rb
@@ -1,24 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-<<<<<<< HEAD
         t.string :name
         t.string :provider
         t.string :uid
         t.string :profile_image
         
-        t.integer :group_id
-        
-        t.references :user, foreign_key: true
+        t.references :group, foreign_key: true
         t.timestamps
-=======
-      t.string :name
-      t.string :provider
-      t.string :uid
-      
-      t.string :profile_image
-      t.timestamps
->>>>>>> origin/master
     end
   end
 end
