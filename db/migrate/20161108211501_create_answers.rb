c0a46705cc9af2c0c188b11d6503d7ebd2110cd8
6ytrenches <miguelmyers8@gmail.com>
2016-11-09 10:11:31 -0500

    answers table

--- db/migrate/20161108211501_create_answers.rb
+++ db/migrate/20161108211501_create_answers.rb
@@ -1,6 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
+      t.boolean :ans
       t.references :user, foreign_key: true
       t.references :question, foreign_key: true
       t.timestamps
