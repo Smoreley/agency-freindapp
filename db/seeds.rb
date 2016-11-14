a5abbc6e52dfaf16cdbd8df09b1e3b24bb9ff7d8
6ytrenches <miguelmyers8@gmail.com>
2016-11-14 14:45:22 -0500

    questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,26 @@
 #
 # Examples:
 
+#how your are feeling
 q1 = Question.create(inquire: "Are you ready for today?", image: "https://i.ytimg.com/vi/ALvep1RsGQ0/hqdefault.jpg")
 q2 = Question.create(inquire: "Do you feel happy?")
 q3 = Question.create(inquire: "Was getting out of bed easy today?")
 q4 = Question.create(inquire: "Do you have a goal today??")
 q5 = Question.create(inquire: "Do you feel motivated?")
-q6 = Question.create(inquire: "Are you ?")
+q6 = Question.create(inquire: "Are you alive?")
+q6 = Question.create(inquire: "do you feel stuck in life?")
+q14 = Question.create(inquire: "Are you feeling adventurous, will you contact your match at the end?")
+q14 = Question.create(inquire: "Are you feeling adventurous?", image: "http://weknowyourdreams.com/images/adventure/adventure-01.jpg")
 q7 = Question.create(inquire: "Have you said everything you needed to say?")
 q8 = Question.create(inquire: "If today was your last day alive, would you be ok with that?")
 q9 = Question.create(inquire: "Are you feeling anxious?", image: "ttps://i.ytimg.com/vi/e2byTLRRl5g/maxresdefault.jpg")
 q10 = Question.create(inquire: "Today is your last day, are you ready?")
-
+#what you did
 q11 = Question.create(inquire: "Did you make your bed today?", image: "https://i.ytimg.com/vi/ALvep1RsGQ0/hqdefault.jpg")
 q12 = Question.create(inquire: "Did you brush your teeth?")
 q13 = Question.create(inquire: "Did you feed your pet or child or water your plant?")
-q14 = Question.create(inquire: "")
-q15 = Question.create(inquire: "?")
-q16 = Question.create(inquire: "?")
-q17 = Question.create(inquire: "?")
-q18 = Question.create(inquire: "?")
+
 q19 = Question.create(inquire: "?", image: "ttps://i.ytimg.com/vi/e2byTLRRl5g/maxresdefault.jpg")
-q20 = Question.create(inquire: "?")
+
+#what will you do
+q6 = Question.create(inquire: "will you share a 10 second stare with a stranger?", image: "https://qph.ec.quoracdn.net/main-qimg-853191037708080ef60f4f9d5fcec6f4?convert_to_webp=true")
